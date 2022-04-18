98df8cebedd72ee26de6513890d29f96b1c48c71
josuesampaiodeoliveira <josue.oliveira@sptech.school>
2022-04-18 09:20:25 -0300

    Arquivos da API que foram baixados do repositorio da SPTech

--- app.js
+++ app.js
@@ -25,9 +25,8 @@ app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter)
 
 app.listen(PORTA, function () {
-    console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
-    Você está rodando sua aplicação em Ambiente de ${process.env.AMBIENTE_PROCESSO} \n
-    \t\tSe "desenvolvimento", você está se conectando ao banco LOCAL (MySQL Workbench). \n
-    \t\tSe "producao", você está se conectando ao banco REMOTO (SQL Server em nuvem Azure) \n
-    \t\t\t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'`);
+    console.log(`Servidor do site está rodando rodando: http://localhost:${PORTA} \n
+    Você está rodando sua aplicação em ${process.env.AMBIENTE_PROCESSO} \n
+    \t\tSe "desenvolvimento", banco local (MySQL Workbench). \n
+    \t\tSe "producao", banco remoto (SQL Server em nuvem Azure)`);
 });
